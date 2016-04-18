83f8f3d9868d690d0a89e67300855d253e31cb33
Yury Mitskevich <mickevich.ja@gmail.com>
2016-04-18 18:40:58 +0300

    Connection to api add ListView add Play Services

--- app/src/main/java/mickevichyura/github/com/mapsplaces/MainActivity.java
+++ app/src/main/java/mickevichyura/github/com/mapsplaces/MainActivity.java
@@ -1,23 +1,52 @@
 package mickevichyura.github.com.mapsplaces;
 
-import android.accounts.AccountManager;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResult;
+import com.google.android.gms.location.places.PlacePhotoResult;
+import com.google.android.gms.location.places.Places;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private GoogleApiClient mGoogleApiClient;
+
+    private ListView listPlaces;
+
+    private ImageView mImageView;
+
     static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     private void pickUserAccount() {
         String[] accountTypes = new String[]{"com.google"};
@@ -32,9 +61,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        listPlaces = (ListView) findViewById(R.id.listPlaces);
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
 
-        pickUserAccount();
-
+        requestPermission();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +78,150 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            HashMap<String, Object> itemHashMap = (HashMap<String, Object>) parent.getItemAtPosition(position);
+            String titleItem = itemHashMap.get(TITLE).toString();
+            String descriptionItem = itemHashMap.get(DESCRIPTION).toString();
+            Toast.makeText(getBaseContext(),
+                    "Вы выбрали " + titleItem + ". " + descriptionItem, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    ResultCallback<PlaceLikelihoodBuffer> cb = new ResultCallback<PlaceLikelihoodBuffer>() {
+        @Override
+        public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+            mPlacesList = new ArrayList<HashMap<String, Object>>();
+            HashMap<String, Object> hm;
+
+            for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                hm = new HashMap<>();
+                hm.put(TITLE, placeLikelihood.getPlace().getName()); // Название
+
+                placePhotosAsync(placeLikelihood.getPlace().getId());
+                hm.put(DESCRIPTION, placeLikelihood.getPlace().getAddress()); // Описание
+                hm.put(ICON, R.drawable.mr_ic_cast_light); // Картинка
+                mPlacesList.add(hm);
+            }
+            SimpleAdapter adapter = new SimpleAdapter(getContext(), mPlacesList,
+                    R.layout.list_item, new String[]{TITLE, DESCRIPTION, ICON},
+                    new int[]{R.id.text1, R.id.text2, R.id.img});
+
+            listPlaces.setAdapter(adapter);
+            listPlaces.setOnItemClickListener(itemClickListener);
+
+            likelyPlaces.release();
+        }
+    };
+
+    private ResultCallback<PlacePhotoResult> mDisplayPhotoResultCallback
+            = new ResultCallback<PlacePhotoResult>() {
+        @Override
+        public void onResult(PlacePhotoResult placePhotoResult) {
+            if (!placePhotoResult.getStatus().isSuccess()) {
+
+                return;
+            }
+            mImageView.setImageBitmap(placePhotoResult.getBitmap());
+        }
+    };
+
+    private void placePhotosAsync(String placeId) {
+        mImageView = (ImageView) findViewById(R.id.mImageView);
+        Places.GeoDataApi.getPlacePhotos(mGoogleApiClient, placeId)
+                .setResultCallback(new ResultCallback<PlacePhotoMetadataResult>() {
+                    @Override
+                    public void onResult(PlacePhotoMetadataResult photos) {
+                        if (!photos.getStatus().isSuccess()) {
+                            return;
+                        }
+
+                        PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
+                        if (photoMetadataBuffer.getCount() > 0) {
+                            photoMetadataBuffer.get(0)
+                                    .getScaledPhoto(mGoogleApiClient, mImageView.getWidth(),
+                                            mImageView.getHeight())
+                                    .setResultCallback(mDisplayPhotoResultCallback);
+                        }
+                        photoMetadataBuffer.release();
+                    }
+                });
+    }
+
+    private Context getContext() {
+        return this;
+    }
+
+    private ArrayList<HashMap<String, Object>> mPlacesList;
+    private static final String TITLE = "title"; // Верхний текст
+    private static final String DESCRIPTION = "description"; // ниже главного
+    private static final String ICON = "icon";  // будущая картинка
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        } else {
+
+
+            PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                    .getCurrentPlace(mGoogleApiClient, null);
+            result.setResultCallback(cb);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                    TextView tv = (TextView) findViewById(R.id.tv);
+                    if (tv != null) {
+                        tv.setText("NO");
+                    }
+                }
+                return;
+            }
+        }
     }
 
     @Override
@@ -68,14 +246,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected void onActivityResult(final int requestCode, final int resultCode,
-                                    final Intent data) {
-        if (requestCode == REQUEST_CODE_PICK_ACCOUNT && resultCode == RESULT_OK) {
-            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-            TextView tv = (TextView) findViewById(R.id.tv);
-            if (tv != null) {
-                tv.setText(accountName);
-            }
-        }
-    }
 }
